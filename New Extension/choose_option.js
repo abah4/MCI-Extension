2130e25e53c2c0926176f05bfdee4046a9ec2a57
abah4 <abah4@umbc.edu>
2019-04-19 13:54:24 -0400

    Update choose_option.js

--- New Extension/choose_option.js
+++ New Extension/choose_option.js
@@ -9,15 +9,11 @@ document.addEventListener("click", function(e) {
 
     var css = "body { border: 5px solid red; }";
     var insertingCSS = browser.tabs.insertCSS({code: css});
-    insertingCSS.then(null, onError);
-
   }
 
   if (e.target.classList.contains("Button2")) {
     var enables = browser.tabs.executeScript({file: "Enable.js"});
     console.log('links should be enabled');
-
-  insertingCSS.classList.remove();
   }
 
 });
