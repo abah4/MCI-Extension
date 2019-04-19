cfdfa9ce8a9330d376b5b069a2d542ab3abcfcbe
abah4 <abah4@umbc.edu>
2019-04-19 13:44:40 -0400

    Update Disable.js

--- New Extension/Disable.js
+++ New Extension/Disable.js
@@ -3,21 +3,8 @@ console.log('Attempting to disable link');
 var links = document.getElementsByTagName("a");
 
 for (var i = 0; i < links.length; i++) {
-        var href = links[i].getAttribute("rel");
-        links[i].removeAttribute("rel");
-        links[i].href = href
-    }
-});
-
-var css = "body { border: 5px solid red; }";
-
-browser.browserAction.onClicked.addListener(() => {
-
-  function onError(error) {
-    console.log(`Error: ${error}`);
-  }
-
-  var insertingCSS = browser.tabs.insertCSS({code: css});
-  insertingCSS.then(null, onError);
-});
+    var href = links[i].href;
+    links[i].setAttribute("rel", href);
+    links[i].href = "javascript:;"
+}
 console.log('Done disabling links');
