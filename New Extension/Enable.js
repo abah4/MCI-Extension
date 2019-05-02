b3329def0ca9b4c2e51817da3f037f93b0ba464d
abah4 <abah4@umbc.edu>
2019-05-02 11:30:28 -0400

    Update Enable.js

--- New Extension/Enable.js
+++ New Extension/Enable.js
@@ -6,6 +6,7 @@ for (var i = 0; i < links.length; i++) {
         var href = links[i].getAttribute("rel");
         links[i].removeAttribute("rel");
         links[i].href = href
+        console.log(href);
     };
-    
+
 console.log('Done enabling links');
