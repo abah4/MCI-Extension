721d31633d5727cd52bf50b68c54d7d724a46f24
Alseny Bah <alsenybah@AB-2.local>
2019-07-29 13:07:31 -0400

    Update

--- test-bfs-dfs/script/test.js
+++ test-bfs-dfs/script/test.js
@@ -137,10 +137,34 @@ function commonAncestor(node1, node2) {
 }
 
 var r1 = document.querySelectorAll('a[href*="https://hangouts.google.com/"]');
-var r2 = document.querySelectorAll('a[href*="https://drive.google.com/u/0/settings/storage?hl=en&utm_medium=onramp&utm_source=gmail"]');
+var r2 = document.querySelectorAll('a[href*="https://drive.google.com/u/0/settings/storage?hl=en&utm_medium=web&utm_source=gmail"]');
 
 var commonParent = commonAncestor(r1[0],r2[0]);
 
+console.log("Common Parent: ");
 console.log(commonParent);
 
-console.log(parents(commonParent));
+console.log("Path from child to parent: ");
+var tree = parents(commonParent); //shown path of the node to the root
+
+console.log(tree);
+
+console.log("****************************************************")
+
+console.log(tree[2].previousElementSibling);
+console.log(tree[2].nextElementSibling);
+
+console.log("****************************************************");
+
+var getSiblings = function (elem) {
+	var siblings = [];
+	var sibling = elem.parentNode.firstChild;
+	for (; sibling; sibling = sibling.nextSibling) {
+		if (sibling.nodeType !== 1 || sibling === elem) continue;
+		siblings.push(sibling);
+	}
+	return siblings;
+};
+
+var siblings = getSiblings(tree[2]);
+console.log(siblings);
